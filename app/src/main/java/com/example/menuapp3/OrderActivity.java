041b0058bb6a14debf40ce930e437e4a6a3dfa6d
RCCR009 <rconejov@ucenfotec.ac.cr>
2020-03-13 22:14:17 -0600

    add order feedback

--- app/src/main/java/com/example/menuapp3/OrderActivity.java
+++ app/src/main/java/com/example/menuapp3/OrderActivity.java
@@ -1,6 +1,7 @@
 package com.example.menuapp3;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -9,25 +10,24 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 
-import Adapter.BreakfastAdapter;
 import Adapter.OrderAdapter;
 import Model.ListBreakfastItem;
 
 public class OrderActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +38,9 @@ public class OrderActivity extends AppCompatActivity {
         //        up button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //float action button
+        fab = findViewById(R.id.fab);
+
         //Busca e inicializa el recyclerView
         recyclerView = findViewById(R.id.rv_order);
         recyclerView.setHasFixedSize(true);
@@ -45,10 +48,11 @@ public class OrderActivity extends AppCompatActivity {
 
         //search the list from localStorage
         SharedPreferences sharedPreferences = OrderActivity.this.getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
-        Gson gson =  new Gson();
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final Gson gson =  new Gson();
         String json = sharedPreferences.getString("task list", null);
         Type type = new TypeToken<ArrayList<ListBreakfastItem>>() {}.getType();
-        ArrayList<ListBreakfastItem> listFromStorage;
+        final ArrayList<ListBreakfastItem> listFromStorage;
 
         //Trea la lista del localStorage
         listFromStorage = gson.fromJson(json, type);
@@ -59,7 +63,21 @@ public class OrderActivity extends AppCompatActivity {
             recyclerView.setAdapter(adapter);
         }
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFinishOrder(v);
+                String jsonAdd = gson.toJson(new ArrayList<ListBreakfastItem>());
+                editor.putString("task list", jsonAdd);
+                editor.apply();
+            }
+        });
 
+    }
+
+    public void openFinishOrder(View view) {
+        Intent intent = new Intent(OrderActivity.this, OrderFeedbackActivity.class);
+        startActivity(intent);
     }
 
 }
